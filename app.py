b5e7d5d2681df1768e716d1a5682831f0e25cfa8
Kavin <immkavin.ofcl@gmail.com>
2026-08-09 02:12:45 +0000

    Add secret key retrieval and update home route message

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 from flask import Flask
+import os
 
 app = Flask(__name__)
 
+secret_key = os.environ.get("SECRET_KEY")
+
 @app.route("/")
 def home():
-    return "Hello! This application is running on PaaS."
+    return f"Hello! This application is running on PaaS. Kavin's secret: {SECRET_KEY}"
 
 if __name__ == "__main__":
     app.run()
